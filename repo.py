ab5ea6b206a9a44e7b15c2c9aef1f54791e99934
grold0123 <172453552+grold0123@users.noreply.github.com>
2025-11-02 08:03:24 +0800

    update

---
+++ repo.py
@@ -0,0 +1,16 @@
+import subprocess
+
+base_dir = 'C:\projects\game_programming_start_to_finish'
+
+args = [
+    ('git add:',['git','add',base_dir]),#add
+    ('git commit:',['git','commit','-m','update']),#commit
+    ('git commit:',['git','push']),#push
+]
+
+for prompt,arg in args:
+    print(f"\n\n{prompt}\n\n")
+    subprocess.run(arg)
+    print(f'\n\nSuccess\n\n')
+
+print("Repository updated")
